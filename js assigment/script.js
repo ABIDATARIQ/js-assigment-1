06da826a5ac8e25643649b75062958fd7cd623bf
ABIDATARIQ <162459792+ABIDATARIQ@users.noreply.github.com>
2024-12-30 23:18:46 +0500

    Add files via upload

---
+++ js assigment/script.js
@@ -0,0 +1,20 @@
+let studentName = "john doe";
+let age = "17 year old";
+let education = " certified moblie application developer";
+alert (studentName);
+alert( age);
+alert(education );
+
+
+let =( "PIZZA\nPIZZ\nPIZ\nPI\nP");
+alert( "PIZZA\nPIZZ\nPIZ\nPI\nP");
+let email = " example@example.com ";
+alert( "my email address is : " + email );
+let message = " i am trying to learn from the book a smarter way to learn javascrit"
+alert( message)
+console.log(message);
+
+
+let design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬" 
+alert(design)
+document.write( design)
